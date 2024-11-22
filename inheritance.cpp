b320c1e51485489a5995a26b6d7ee15856647fc7
chetan <vaishnavchetan701@gmail.com>
2024-11-23 00:34:20 +0530

    code updated

--- inheritance.cpp
+++ inheritance.cpp
@@ -1,41 +1,57 @@
-
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class LibraryItem{
-    protected:
+class LibraryItem {
+protected:
     string type;
-    static int id;
-    public:
-    LibraryItem(){
-        type="none";
+    static int id; //it will shared to all instances of this class
+public:
+    LibraryItem() {
+        type = "none";
         id++;
-
     }
-    LibraryItem(string n):type(n){id++;};
-    void display(){
-        cout<<"\nType: "<<type<<"\nId: "<<id<<endl;
+    LibraryItem(string n) : type(n) {
+        id++;
+    }
+    virtual void display() const { 
+        cout << "\nType: " << type << "\nId: " << id << endl;
     }
 };
-int LibraryItem :: id=0;
-class Books:public LibraryItem{
+int LibraryItem::id = 0; 
+
+class Books : public LibraryItem {
     string name;
     int pages;
-    public:
-    
+public:
+    Books() : LibraryItem("book"), name("Unknown"), pages(0) {}
+    Books(string n, string m, int p) : LibraryItem(n), name(m), pages(p) {}
+    void display() const override { 
+        cout << "\nType: " << type << "\nId: " << id
+             << "\nName: " << name << "\nPages: " << pages << endl;
+    }
 };
 
-class DVDs{
-
+class DVDs : public LibraryItem {
+    string title;
+    double duration; 
+public:
+    DVDs() : LibraryItem("DVD"), title("Untitled"), duration(0.0) {}
+    DVDs(string t, double d) : LibraryItem("DVD"), title(t), duration(d) {}
+    void display() const override {
+        cout << "\nType: " << type << "\nId: " << id
+             << "\nTitle: " << title << "\nDuration: " << duration << " hours" << endl;
+    }
 };
-int main(){
-    cout<<"Helloworld!";
-    cout<<"This is new line";
-    cout<<"This is new line";
-    cout<<"This is new line";
-    cout<<"This is new line";
-    cout<<"This is new line";
 
-    
+int main() {
+    LibraryItem* l;
+    Books b("book", "The Boys", 456);
+    l = &b;
+    l->display(); // Will now call Books::display() due to polymorphism
+
+    DVDs d("The Matrix", 2.5);
+    l = &d;
+    l->display(); // Demonstrates polymorphism with DVDs::display()
+
+    return 0;
 }

4c70b408b94f3427d3278362aa8119b2719b98a2
Chetan Bairagi <136821832+Chetan0078@users.noreply.github.com>
2024-11-22 23:33:58 +0530

    Add files via upload

---
+++ inheritance.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class LibraryItem{
+    protected:
+    string type;
+    static int id;
+    public:
+    LibraryItem(){
+        type="none";
+        id++;
+
+    }
+    LibraryItem(string n):type(n){id++;};
+    void display(){
+        cout<<"\nType: "<<type<<"\nId: "<<id<<endl;
+    }
+};
+int LibraryItem :: id=0;
+class Books:public LibraryItem{
+    string name;
+    int pages;
+    public:
+    
+};
+
+class DVDs{
+
+};
+int main(){
+    cout<<"Helloworld!";
+    
+}

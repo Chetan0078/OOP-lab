af0c70a56e4199dea414f511162f3a733a4f64f4
chetan <vaishnavchetan701@gmail.com>
2024-12-25 15:36:53 +0530

    file is updated

--- ClassLearings/reversenum.cpp
+++ ClassLearings/reversenum.cpp
@@ -1,33 +1,40 @@
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Maths{
+class Maths
+{
     int num;
-    public:
-    Maths(){
-        num=0;
+
+public:
+    Maths()
+    {
+        num = 0;
     }
-    void setNum(int n){
-        num=n;
+    void setNum(int n)
+    {
+        num = n;
     }
-    int getNum(){
+    int getNum()
+    {
         return num;
     }
-    int reverseNum(int num){
-        int rem,rev=0;
-        while (num>0){
-            rem=num%10;
-            num=num/10;
-            rev=rev*10+rem;
+    int reverseNum(int num)
+    {
+        int rem, rev = 0;
+        while (num > 0)
+        {
+            rem = num % 10;
+            num = num / 10;
+            rev = rev * 10 + rem;
         }
         return rev;
     }
 };
-int main(){
+int main()
+{
     Maths a1;
     int num;
-    cin>>num;
+    cin >> num;
     a1.setNum(num);
-    cout<<endl;
-    cout<<a1.reverseNum(num);
+    cout << endl;
+    cout << a1.reverseNum(num);
 }
